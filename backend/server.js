4bc43e831acc5de8840d67d1523a0398115f9fdb
abstxn <108669111+abstxn@users.noreply.github.com>
2023-05-22 19:30:19 +0800

    Implement live feed using sockets

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,35 @@
-import express from "express"
+import express, { json } from "express"
 import RedditParser from "./services/reddit-parser.js"
+import http from "http"
+import { Server } from "socket.io"
 
 const app = express()
 const redditParser = new RedditParser()
+const server = http.createServer(app)
+const io = new Server(server)
 
-app.get("/", async (req, res) => {
-    res.status(200).json(await redditParser.all())
+let feed = null
+
+io.on("connection", socket => {
+    console.log("Client connected")
+
+    setInterval(async () => {
+        feed = await redditParser.all()
+        socket.emit("feed update", feed)
+    }, 5 * 1000)
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected")
+    })
 })
 
-app.listen(3000)
+app.get("/", async (req, res) => {
+    if (!feed) feed = await redditParser.all()
+
+    res.status(200).json(feed)
+})
+
+const port = 3000
+server.listen(port, () => {
+    console.log(`Server listening to port ${port}`)
+})
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
 import Feed from "../components/Feed"
+import PingListener from "../components/PingListener"
+import io from "socket.io-client"
 
 export default function App() {
     const [feed, setFeed] = useState({})
@@ -9,10 +11,17 @@ export default function App() {
         axios.get("http://localhost:3000")
             .then(res => setFeed(res.data))
             .catch(err => console.log(err))
+        
+        const socket = io("http://localhost:3000")
+        socket.on("feed update", (newFeed) => {
+            setFeed(newFeed)
+        })
+        return () => socket.disconnect()
     }, [])
 
     return (
         <>
+            <PingListener />
             <h1>News Feed</h1>
             <Feed feed={feed} />
             <pre>{JSON.stringify(feed, null, 2)}</pre>
