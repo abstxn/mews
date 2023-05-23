73f81d2653d14a8c13d9aee323caaf1bf071ef98
abstxn <108669111+abstxn@users.noreply.github.com>
2023-05-23 09:40:58 +0800

    Page layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,9 +23,44 @@ export default function App() {
     }, [])
 
     return (
-        <div className="container">
-            <h1>News Feed</h1>
-            <Feed feed={feed} />
-        </div>
+        <>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container">
+                    <a class="navbar-brand" href="#">Mews</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <div class="container mt-4">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="list-group">
+                            <a href="#" class="list-group-item list-group-item-action">Feed 1</a>
+                            <a href="#" class="list-group-item list-group-item-action">Feed 2</a>
+                            <a href="#" class="list-group-item list-group-item-action">Feed 3</a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-9">
+                        <Feed feed={feed} />
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
